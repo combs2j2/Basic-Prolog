a0fd3d888f8602e7bd4da8e5ff2d135010134dae
combs2j2 <56417001+combs2j2@users.noreply.github.com>
2021-11-29 12:20:41 -0500

    Update mergeSort.pl

--- mergeSort.pl
+++ mergeSort.pl
@@ -1,5 +1,11 @@
 split(List, FirstHalf, SecondHalf) :- append(FirstHalf, SecondHalf, List), length(List, N), Half is N/2, ceiling(Half, HalfCeiling), Rest is N-HalfCeiling, length(FirstHalf, HalfCeiling), length(SecondHalf, Rest), !.
 
+merge([], [], []).
+merge(List, [], List).
+merge([], List, List).
+merge([X|Xs], [Y|Ys], [X,Y|Result]) :- Y >= X, merge(Xs, Ys, Result).
+merge([X|Xs], [Y|Ys], [Y,X|Result]) :- Y < X, merge(Xs, Ys, Result).
+
 mergeSort([X], [X]).
 mergeSort([], []).
 mergeSort(List, Result) :- splitInHalf(List, FirstHalf, SecondHalf), mergeSort(FirstHalf, FirstSorted), mergeSort(SecondHalf, SecondSorted), merge(FirstSorted, SecondSorted, Result).
